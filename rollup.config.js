28641ea65695646535cfe98ee18e05a60c5dd932
embers <935267851@qq.com>
2024-02-02 14:24:51 +0800

    update code

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+
+
+export default [
+	{
+		input: 'src/index.js',
+		output: {
+			name:"datafixers",
+			dir: "lib",
+			format: 'es',
+			preserveModules: true, 
+			preserveModulesRoot: 'src',
+		},
+	
+	},
+];
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { BitSet } from "./bitSet";
